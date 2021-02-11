3919baef4593b806b98ce11f180f44ed48e8a7e2
Takahiro Morishita <tmorishita@stsci.edu>
2021-02-11 18:07:47 -0500

    clean code

--- detect_ghost_image3.py
+++ detect_ghost_image3.py
@@ -3,8 +3,8 @@ import os
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-
 import sys
+
 DIR_CODE = './code/'
 sys.path.append(DIR_CODE)
 from utils import get_gap,get_ghost,str2bool
@@ -16,40 +16,39 @@ from astropy.wcs import WCS
 from astroquery.vizier import Vizier
 
 
-def tweak_imaege2(id_gst, infile, segfile, outfile=None, DQ_SET=1):
+if __name__ == "__main__":
     '''
-    This script tweaks image2 DQ arrays, by making positions of detected ghost DQ=1 ("Do not use").
+    Purpose:
+    ========
+    This script is for detecting ghost in Image3 step products.
+    There are two algorithms used to flag possible ghost images.
+
+    1. Root method: Look into detected sources in Image3 catalog, predict positions of ghosts based on GAP, 
+    and then confirm if the source is consistent with the prediction.;
+
+    2. Catalog method: Retrieve bright sources from all sky catalogs (here GAIA, through astroquery),
+    then apply the same method as for root method to predict and confirm any sources in image3 catalog as ghosts.
 
     Return:
     =======
-    Save fits files with _gsf.fits
-    '''
-    if not outfile == None:
-        outfile = infile.replace('.fits','_gst.fits')
+    A copy of the source catalog, with ghost flag added.
 
-    os.system('cp %s %s'%(infile, outfile))
-    
-    seg = fits.open(segfile)[0].data
-    with fits.open(outfile, mode='update') as hdul:
-        for id in id_gst:
-            con = np.where(seg == id)
-            hdul['DQ'].data[con] = DQ_SET
-        
-        hdul.flush()  # changes are written back to original.fits
-
-
-if __name__ == "__main__":
     '''
-    '''
+
     parser = argparse.ArgumentParser(description='Run the NIRISS ghost detection script.')
+    parser.add_argument('input_image', metavar='input_image', type=str, nargs=1, help='Input image to be assessed.')
+    parser.add_argument('input_catalog', metavar='input_catalog', type=str, nargs=1, help='The source catalog derived with input_image.')
     parser.add_argument('--f_verbose',default=False,help='Print comments.', type=str2bool)
-    parser.add_argument('--rlim',default=10,help='Ghost search radius (pixel)', type=float)
-    parser.add_argument('--frac_ghost',default=0.01,help='Flux fraction for ghosts', type=float)
-    parser.add_argument('--f_tweak_imaege2',default=False,help='Tweak Image2 products.', type=str2bool)
+    parser.add_argument('--rlim',default=10,help='Search radius for ghost around the predicted position (in pixel).', type=float)
+    parser.add_argument('--frac_ghost',default=0.01,help='Flux fraction for ghosts.', type=float)
     parser.add_argument('--o',default='./',help='Output directory.', type=str)
-    #parser.add_argument('--filt',default='f150w',help='Flux fraction for ghosts', type=float)
+    parser.add_argument('--f_tweak_imaege2',default=False,help='Tweak Image2 products.', type=str2bool)
+    parser.add_argument('--f_mirage',default=True,help='Is dataset created by Mirage?', type=str2bool)
     args = parser.parse_args()
-    f_mirage = True
+
+    # Flags for analysis method;
+    f_rootmethod = True
+    f_gsc = True
 
     f_verbose = args.f_verbose
     rlim = args.rlim
@@ -59,33 +58,19 @@ if __name__ == "__main__":
     if not os.path.exists(DIR_OUT):
         os.mkdir(DIR_OUT)
 
-    '''
-    f_verbose = True
-    rlim = 10 # pixel
-    frac_ghost = 0.01
-    f_tweak_imaege2 = True
-    #filt = 'f150w'
-    '''
-
     # Ghost detection must be done in distortion corrected frame
-    '''
-    DIR_DATA = './reduced/'
-    file_root = 'jw01085001001_01101'
-    infiles = ['%s/%s_00001_nis_cal.fits'%(DIR_DATA, file_root), \
-            '%s/%s_00006_nis_cal.fits'%(DIR_DATA, file_root), \
-            '%s/%s_00007_nis_cal.fits'%(DIR_DATA, file_root), \
-            '%s/%s_00008_nis_cal.fits'%(DIR_DATA, file_root)]
-    '''
     DIR_DATA = './'
-    file_root = 'l3_nis_test_f150w'
-    infiles = ['%s/%s_i2d.fits'%(DIR_DATA, file_root)]
-    # Catalog;
-    files_cat = [infile.replace('_i2d.fits','_cat_man.ecsv') for infile in infiles]
 
-    for ff,infile in enumerate(infiles[:1]):
-        '''
-        '''
-        
+    # Image;
+    infiles = [args.input_image]
+    # Catalog;
+    #files_cat = [infile.replace('_i2d.fits','_cat_man.ecsv') for infile in infiles]
+    files_cat = [args.input_catalog]
+    
+    for ff,infile in enumerate(infiles[:]):
+
+        file_root = infile.replace('.fits','')
+
         # Read header;
         hd = fits.open(infile)[0].header
         hd1 = fits.open(infile)[1].header
@@ -95,9 +80,9 @@ if __name__ == "__main__":
         YOFFSET = hd['YOFFSET']
         CDELT1 = hd1['CDELT1']
         
-        if f_mirage:
-            # image size difference;
-            # May be specific only to mirage data.        
+        if args.f_mirage:
+            # Add shift for image size difference.
+            # This is due to the fact that ghosts were added in the seed image dimention, whereas analysis is done in i2d image.
             xshift = (2048-hd1['NAXIS1'])/2.
             yshift = (2048-hd1['NAXIS2'])/2.
         else:
@@ -119,9 +104,10 @@ if __name__ == "__main__":
         fw = open('%s/ghs_%s.reg'%(DIR_OUT,file_root), 'w')
         fw.write('# xgst ygst\n')
 
-        if True:
-            # No1; Root method
-            # Look into detected sources in Image2 catalog;
+        ##################
+        # No1; Root method
+        ##################
+        if f_rootmethod:
             gst = get_ghost(fd_cat['xcentroid'][:].value, fd_cat['ycentroid'][:].value, \
                             flux=fd_cat['source_sum'][:], filt=pupil,\
                             xshift=xshift, yshift=yshift)
@@ -153,13 +139,11 @@ if __name__ == "__main__":
                     
                     prob_gst[ii] = prob_pos * prob_flux
 
-                #else:
-                #if f_verbose and fd_cat['id'][ii] == 490:
-                #print(gst)
-                #print('No counterpart is for %d found: r=%.2f>%.2f pixel'%(fd_cat['id'][ii], rtmp[iiy], rlim))
-
-        if True:
-            # No2; Catalog method
+        #####################
+        # No2; Catalog method
+        #####################
+        if f_gsc:
+            from jwst import datamodels
             fd = fits.open(infile)[1].data
             hd = fits.open(infile)[1].header        
             RA = hd['CRVAL1']
@@ -169,65 +153,55 @@ if __name__ == "__main__":
             
             # Astro query;
             Vizier.ROW_LIMIT = 1000
-            # To cover pickoff mirror;
-            wid_extra = 00 # This is from e-mail exchange with Kevin; to be confirmed after flight operation.
-            width_detector = 2048 + wid_extra
-            pixel = 0.06 # arcsec / pixel
 
+            # To cover pickoff mirror;
+            wid_extra = 200 # This is from e-mail exchange with Kevin; to be confirmed after flight operation.
+            width_detector = 2048 + wid_extra
             Vizier.ROW_LIMIT = 1000
+
             result = Vizier.query_region(coord.SkyCoord(ra=RA, dec=DEC,
                                                         unit=(u.deg, u.deg),
                                                         frame='icrs'),
-                                        width='%.1fs'%(width_detector*pixel), 
+                                        width='%.1fs'%(width_detector*CDELT1), 
                                         catalog=["I/345/gaia2"], 
                                         column_filters={'Gmag': '<20'})
                                         #catalog=["GSC"]) # GSC has some issues..
 
             # Get ghost positions;
             # Input magnitude;
-            GSmag = result[0]['Gmag']
+            mvega2ab = -0.08
+            GSmag = result[0]['Gmag'] + mvega2ab
             magzp = 25.0
-            flux_GS = 10**(-(GSmag-magzp)/(2.5))
+            flux_GS = 10**(-(GSmag-magzp)/(2.5)) # Fnu with magzp
 
             RA_key = 'RA_ICRS'
             DE_key = 'DE_ICRS'
             #RA_key = 'RAICRS'
             #DE_key = 'DEICRS'
-
-            if False:
-                # Save;
-                result[0].write('2mass.dat', format='ascii')
             
-            # Get pixel position of 2mass sources;
-            #w = WCS(hd,naxis=2)
-            #x, y = w.wcs_world2pix(result[0][RA_key],result[0][DE_key],0)
-            # This needs to be distortion-uncorrected format;
-            from jwst import datamodels
+            # Get pixel position of GAIA sources;
             im = datamodels.open(infile)
             x, y = im.meta.wcs.backward_transform(result[0][RA_key],result[0][DE_key])
             id_pub = np.arange(0,len(x),1)
 
-            if True:
+            if False:
                 # Add manually added sources to the query result;
                 ra_ext = np.asarray([82.74528081, 82.74549915])
                 de_ext = np.asarray([-68.79706061, -68.79157048])
                 x_ext, y_ext = im.meta.wcs.backward_transform(ra_ext, de_ext)
                 plt.scatter(x_ext, y_ext, marker='o', s=10, edgecolor='r', facecolor='none', label='')
                 flux_GS_ext = [100,100]
-                #gst_ext = get_ghost(x_ext, y_ext, flux=flux_GS_ext, filt=pupil, xshift=xshift, yshift=yshift)
                 x = np.append(x,x_ext)
                 y = np.append(y,y_ext)
                 flux_GS = np.append(flux_GS, flux_GS_ext)
 
-            gst_cat = get_ghost(x, y, flux=flux_GS, filt=pupil, xshift=xshift, yshift=yshift)
-            #plt.scatter(gst_cat[0], gst_cat[1], marker='o', s=30, edgecolor='lightgreen', color='', label='GSC sources')
-            
+
+            gst_cat = get_ghost(x, y, flux=flux_GS, filt=pupil, xshift=xshift, yshift=yshift)            
             for ii in range(len(flag_gst[:])):
-                if flag_gst[ii] != 1: # If not ghost flagged above.
+                if flag_gst[ii] != 1: # If the source has not been flagged above yet.
+
                     # This is because photutils is 0-based, while ds9 is not.
                     shift = 1.0 
-                    #if fd_cat['source_sum'][ii]>100:
-                    #    fw.write('%.3f %.3f\n'%(gst[0]+shift, gst[1]+shift))
 
                     # Check the position:
                     rtmp = np.sqrt( (fd_cat['xcentroid'].value[ii] - gst_cat[0])**2 + (fd_cat['ycentroid'].value[ii] - gst_cat[1])**2 )
@@ -235,12 +209,11 @@ if __name__ == "__main__":
 
                     if rtmp[iiy] < rlim: # and (fd_cat['source_sum'][ii] - fd_cat['source_sum'][iiy])>0 :
                         id_gst[ii] = fd_cat['id'][ii]
-                        flag_gst[ii] = 1 
+                        flag_gst[ii] = 1
                         prob_pos = np.exp(-0.5 * rtmp[iiy]**2)
                         residual = np.abs(fd_cat['source_sum'][ii]*frac_ghost - flux_GS[iiy])
                         expectation = fd_cat['source_sum'][ii]*frac_ghost
                         prob_flux = np.exp(-0.5 * residual**2/expectation)
-                        
                         prob_gst[ii] = prob_pos * prob_flux
 
 
@@ -249,21 +222,20 @@ if __name__ == "__main__":
 
         # Save result;
         if True:
-            suff = infile.split('/')[-1].replace('.fits','')
-
             fd_sci = fits.open(infile)[1].data
             plt.imshow(fd_sci, vmin=0, vmax=1)
 
             # All sources;
             plt.scatter(fd_cat['xcentroid'].value, fd_cat['ycentroid'].value, marker='o', s=30, edgecolor='cyan', color='', label='i2d sources')
+
             # Public;
             if False:
                 plt.scatter(x, y, marker='o', s=30, edgecolor='lightgreen', color='', label='GSC sources')
             
-            fw_cat = open('%s/ghost_detected_cat_%s.txt'%(DIR_OUT,suff),'w')
+            fw_cat = open('%s/ghost_detected_cat_%s.txt'%(DIR_OUT, file_root),'w')
             fw_cat.write('# id raghs decghs xghs yghs\n')
 
-            fw = open('%s/ghost_detected_%s.reg'%(DIR_OUT,suff),'w')
+            fw = open('%s/ghost_detected_%s.reg'%(DIR_OUT, file_root),'w')
             fw.write('# xghs yghs\n')
 
             f_label = True
@@ -278,6 +250,7 @@ if __name__ == "__main__":
                         f_label = False
                     else:
                         label = ''
+
                     plt.scatter(xghs, yghs, marker='s', s=30, edgecolor='r', color='', label=label)
                     shift = 1.0 # This is because photutils is 0-based, while ds9 is not.
                     fw.write('%.3f %.3f\n'%(xghs+shift, yghs+shift))
@@ -287,20 +260,19 @@ if __name__ == "__main__":
 
             # Plot GAP:
             gaps = get_gap(pupil)
-            plt.scatter(gaps[0], gaps[1], marker='x', s=30, color='orange', label='GAP')#, color='')
-            
+            plt.scatter(gaps[0], gaps[1], marker='x', s=30, color='orange', label='GAP')
             fw_cat.close()
             fw.close()
             plt.legend(loc=0)
-            plt.savefig('%s/results_%s.png'%(DIR_OUT,suff), dpi=300)
+            plt.savefig('%s/results_%s.png'%(DIR_OUT,file_root), dpi=300)
             plt.close()
-        
 
+        '''
+        from utils import tweak_imaege2
+        # This is for future implementation;
         if f_tweak_imaege2:
             con = (flag_gst==1)
-            print(id_gst[con])
             segfile = infile.replace('.fits', '_seg.fits')
             outfile = infile.replace('.fits', '_gst.fits')
             tweak_imaege2(id_gst[con], infile, segfile, outfile=outfile)
-
-
+        '''
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 import argparse
-
 from astropy.io import fits,ascii
 
 def str2bool(v):
@@ -93,87 +92,24 @@ def get_ghost(x, y, flux=None, filt='F200W', shift=0, xshift=0, yshift=0, file_g
     return xgs,ygs,flux_gs,x,y,flux
 
 
-
-
-def run_mirage(pfile, DIR_INP='./files/', f_plot=True):
+def tweak_imaege2(id_gst, infile, segfile, outfile=None, DQ_SET=1):
     '''
-    Purpose:
-    ========
-    Create NIRISS scene with ghost.
+    This script tweaks image2 DQ arrays, by making positions of detected ghost DQ=1 ("Do not use").
 
-    Input:
-    ======
-    pfile : yaml file. No configuration for ghost is needed.
-    f_plot : Bool, for plot.
+    Return:
+    =======
+    Save fits files with _gsf.fits
     '''
-    #from utils import get_gap,get_ghost
+    if not outfile == None:
+        outfile = infile.replace('.fits','_gst.fits')
 
-    ghost_image = '%s/ghost_cen.fits'%DIR_INP
-
-    # 1.1 Run;
-    print('Running catalog_seed_image for {}'.format(pfile))
-    imseeds = []
-    cat = catalog_seed_image.Catalog_seed(offline=False)
-    cat.paramfile = pfile
-
-    cat.make_seed()
-    imseeds.append(cat.seed_file)
-
-    # 1.2. Get output source catalog;
-
-    # Taken From Mirage;
-    source_type = 'pointsources'
-    psfile = cat.params['Output']['file'][0:-5] + '_{}.list'.format(source_type)
+    os.system('cp %s %s'%(infile, outfile))
     
-    # This should be a list of real sources;
-    tab_source = ascii.read(psfile, comment = '#', header_start=2) #, data_start=0)
+    seg = fits.open(segfile)[0].data
+    with fits.open(outfile, mode='update') as hdul:
+        for id in id_gst:
+            con = np.where(seg == id)
+            hdul['DQ'].data[con] = DQ_SET
+        
+        hdul.flush()  # changes are written back to original.fits
 
-
-    # _s represents source, as opposed to _gs for ghost;
-    mag_s = tab_source['magnitude']
-
-    # Let's focus on bright sources
-    con = (mag_s<15)
-
-    id_s = tab_source['Index'][con]
-    xpix_s = tab_source['pixel_x'][con]
-    ypix_s = tab_source['pixel_y'][con]
-    ra_s = tab_source['RA_degrees'][con]
-    dec_s = tab_source['DEC_degrees'][con]
-    count_s = tab_source['counts/sec'][con]
-
-
-    # Use median for GAP coordinate and flux ratio;
-    filter = cat.params['Readout']['pupil'] #'F150W'
-
-    # Coordinates and flux for ghosts:
-    xgs,ygs,flux_gs,xin,yin,fluxin = get_ghost(xpix_s, ypix_s, \
-                                            flux=count_s, filt=filter,\
-                                            file_gap=file_gap)
-
-    # Assign new ids for Ghost:
-    # Here, id+10000.
-    t = Table()
-    t['idgs']= id_s + 100000
-    t['xgs'] = xgs
-    t['ygs'] = ygs
-    t['counts/sec'] = flux_gs
-
-
-    if f_plot:
-        plt.style.use('dark_background')
-
-        fig = plt.figure(figsize=(8,8))
-        fig.subplots_adjust(top=0.94, bottom=0.12, left=0.14, right=0.98, hspace=0.12, wspace=0.2)
-
-        # Source;
-        plt.scatter(xpix_s,ypix_s,marker='o',c='w',alpha=0.5,s=count_s/1000)
-
-        xdet = 2048
-        ydet = 2048
-
-        plt.xlim(0,xdet)
-        plt.ylim(0,ydet)
-        plt.title('Source')
-
-        plt.savefig('ps.png', dpi=150)
